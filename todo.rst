155c40287a826fc4d2911b46af1a8f40348452e6
Moisès Gómez Girón <moisesgomezgiron@gmail.com>
2013-05-23 19:44:21 +0200

    reconsidered todo list and some moving

--- todo.rst
+++ todo.rst
@@ -15,11 +15,11 @@ Currently:
     Current version v01 scores clearly better than qwerty and
     dvorak.es:
 
-    Following computations at util/frequencies.ods:
+    Following computations at frequencies.ods:
 
-    -   dvorak es improves qwerty by 5,63%
+    -   dvorak.es improves qwerty by 5,63%
     -   tcat.v01 improves qwerty by 17,74%
-    -   tcat.v01 improves dvorak es by 12,83%
+    -   tcat.v01 improves dvorak.es by 12,83%
 
     Most of the code in util/ should be revised if anyone wants to
     perform some automated scoring. I'd suggest to use the ods file to
@@ -31,34 +31,11 @@ Now, the ToDo list include:
    https://help.ubuntu.com/community/Howto%3A%20Custom%20keyboard%20layout%20definitions
    to learn which are the customization possibilities
 
-#. include fixed keys possibility for numbers and so
+#. define further checks that allow discriminate layouts
 
-#. define a class that allows mapping preferred fingers for key
+   - hjkl vim's keys
 
-#. define a class that allows mapping a comfort key
-
-#. define a class that allows mapping a layout
-
-#. develop a program that, given comfort key map, a key-frequency
-   a pair of keys-frequency, and a layout, returns
-   the comfort score of the layout
-
-#. develop a program that, given comfort key map, a layout and a text
-   returns the comfort score of the text
-
-#. develop a program to test whether a given layout suits vi hjkl keys
-   usage
-
-#. develop a program that, given a comfort key map, a key-frequency
-   and a pair of keys-frequency, generates the N better scored comfort
-   layouts (ordered by better to worse)
-
-#. develop a program that, given comfort key map, a list of layouts
-   and a test, returns the comfort score of the text for each layout
-   (ordered by better to worse)
-
-#. develop a program that is able to calculate the comfort score the
-   text from a given url for a given list of layouts.
+   - pair frequencies
 
 #. program that, given a layout, generates xkb files
 
---
+++ util/comfortkeys.py
@@ -0,0 +1,96 @@
+#! /usr/bin/env python
+# encoding: utf-8
+#
+# This class implements a comfort keys layout
+# It consists on a comfort index for each key in a keyboard
+#
+# Keys considered are (in a qwerty layout):
+#    qwertyuiop`+
+#    asdfghjklñ´ç
+#   <zxcvbnm,.-
+
+# On these premises, keys are numbered from left to right and from top to
+# bottom as follow:
+#      00 01 02 03 04 05 06 07 08 09 10 11
+#      12 13 14 15 16 17 18 19 20 21 22 23
+#      24 25 26 27 28 29 30 31 32 33 34
+
+# Comfort key index can be defined in a text file containing at least 35
+# positive integer values (white spaces and extra integers ignored) and will be
+# considered in the corresponding order previously defined.
+#
+# Default values are all keys set to index 1
+#
+# Note: this code does not pursues robustness in this first attempt
+# Therefore, it will break if, for instance, you pass it a non-existent file
+#
+import sys, os.path
+#
+class ConfortKeys:
+    def __init__(self, filename=None):
+        self.numberofkeys = 35
+        if filename:
+            self.loadConfortKeys(filename)
+        else:
+            self.setDefaultConfortKeys()
+
+    def loadConfortKeys(self, filename):
+        values = open(filename).read().split()
+        self.keys = dict(enumerate([int(x) for x in values[:self.numberofkeys]]))
+
+    def setDefaultConfortKeys(self):
+        self.keys = dict([(x, 0) for x in range(self.numberofkeys)])
+
+    def comfortIndex(self, key):
+        """ returns the comfort index for key """
+        return self.keys[key]
+
+    def lowestIndex(self):
+        """ returns the lowest comfort index """
+        return min(self.keys.values())
+
+    def highestIndex(self):
+        """ returns the highest comfort index """
+        return max(self.keys.values())
+
+    def keysOfIndex(self, index):
+        """ returns the keys that share the index """
+        return [ k for k in self.keys if self.keys[k]==index]
+
+    def scoreLayout(self, layout, frequency):
+        """ given a layout and a symbol frequency, 
+        this function computes the comfort score based on the
+        comfort key indexes """
+        score = 0.0
+        for k in self.keys:
+            comfort = float(self.keys[k])
+            symbol = layout.symbols[k]
+            freq = float(frequency.freq[symbol])
+            score += 10 * comfort * (1 - freq)
+        return score
+
+
+#
+def main():
+    """ performs a simple test """
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+    else:
+        filename = raw_input("Filename of the comfort key indexes: ")
+    if os.path.isfile(filename):
+        ck = ConfortKeys(filename)
+        print "Loaded %s"%filename
+    else:
+        ck = ConfortKeys()
+        print "Loaded defaults"
+
+    li = ck.lowestIndex()
+    hi = ck.highestIndex()
+    print "Lowest index: %s"%li
+    for i in range(li, hi):
+        print "keys of index %s: %s"%(i, ck.keysOfIndex(i))
+    return 0
+#
+if __name__=="__main__":
+    sys.exit(main())
+
