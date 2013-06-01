37be0b6c4745af1a44d93edc869c3ae807af9633
Moisès Gómez Girón <moisesgomezgiron@gmail.com>
2013-06-01 12:46:02 +0200

    added some more layouts

--- util/score_text.py
+++ util/score_text.py
@@ -15,6 +15,9 @@ from BeautifulSoup import BeautifulSoup
 from KeyDistance import KeyDistance
 from Layout import KeyboardLayout
 from HandDistrib import HandDistribution
+from tempfile import NamedTemporaryFile
+#
+_FILENAME="/tmp/fitxer_normalitzat.txt"
 #
 def normalitza_lletra(lletra):
     """ retorna la lletra normalitzada. És a dir, elimina els
@@ -57,17 +60,23 @@ def normalitza_lletra(lletra):
         lletra = ['<']
     return lletra
 #
-def genera_parells(text, layout):
-    """ genera els parells de lletres de raw i retorna les posicions
+def genera_parells(layout):
+    """ genera els parells de lletres del fitxer i retorna les posicions
         corresponents al layout """
+    fd = codecs.open(_FILENAME, "r", "utf-8")
     parells = list()
-    for i in range(len(text)-1):
-        x, y = text[i], text[i+1]
-        if x <> y and i <> ' ':
+    prev = ' '
+    while True:
+        ch = fd.read(1)
+        if not ch:
+          break
+        if ch <> prev and ch <> ' ':
             symbols = layout.symbols
-            index_x = symbols.index(x) if x in symbols else 0
-            index_y = symbols.index(y) if y in symbols else 0
+            index_x = symbols.index(prev) if prev in symbols else 0
+            index_y = symbols.index(ch) if ch in symbols else 0
             parells.append((index_x, index_y))
+        prev = ch
+    fd.close()
     return parells
 
 def score_hand_distrib(hd, k1, k2):
@@ -79,30 +88,30 @@ def score_hand_distrib(hd, k1, k2):
         res = 0
     return res
 #
-def score_layout(layout, kd, hd, text):
-    parells = genera_parells(text, layout)
+def score_layout(layout, kd, hd):
+    parells = genera_parells(layout)
     score_distance = score_hands = 0
     for p in parells:
         score_distance += kd.distances.get(p, 0)
         score_hands += score_hand_distrib(hd, *p)
     return score_distance, score_hands
 #
-def analyze_text(layouts, kd, hd, source, text):
+def analyze_text(layouts, kd, hd, source):
     """ creates a report for text considering scores for each layout """
     min_distance = min_hand = sys.maxint
     results = dict()
     for l in layouts:
-        score = score_layout(layouts[l], kd, hd, text)
+        score = score_layout(layouts[l], kd, hd)
         min_distance = min(min_distance, score[0])
         min_hand = min(min_hand, score[1])
         results[l]=score
 
     sorted_results = sorted(results.iteritems(), key=operator.itemgetter(1))
-    print "\nSource (%s sym): %s"%(len(text), source)
+    print "\nSource: %s"%source
     for l, score in sorted_results:
         score_distance, score_hands = score
         pcent_distance = 100*(score_distance - min_distance) / min_distance
-        pcent_hand = 100*(score_hands - min_hand) / min_hand
+        pcent_hand = 100*(score_hands - min_hand) / min_hand if min_hand <> 0 else 0
         print "\t[%s]:\tdistance=%10s (%0.2f%%)\thands=%s (%4.2f%%)"%(l, score_distance, pcent_distance, score_hands, pcent_hand)
 #
 def load_layouts():
@@ -139,12 +148,14 @@ def main():
             except:
                 print "ERROR processing %s url"%f
         if raw:
-            lletres = []
+            print "Loaded raw"
+            tf = codecs.open(_FILENAME, "w", "utf-8")
             for ch in raw:
-                lletres += normalitza_lletra(ch)
+                tf.write("".join(normalitza_lletra(ch)))
+            tf.close()
             raw = None
-            text = "".join(lletres)
-            analyze_text(layouts, kd, hd, f, text)
+            print "Encoded raw"
+            analyze_text(layouts, kd, hd, f)
 #
 if __name__=="__main__":
     sys.exit(main())
